4e3c50ca95c74e0ef55f22bbf39e605e49946200
Talentedfalcon <82446314+Talentedfalcon@users.noreply.github.com>
2026-05-27 19:41:00 +0530

    seperated events from main file and added command cooldowns

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Events, GatewayIntentBits, MessageFlags, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 
 const client = new Client({ intents:[GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, (readyClient)=>{
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
-
 
 client.commands = new Collection();
-
+client.cooldowns = new Collection();
 const foldersPath = path.join(__dirname,'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -29,33 +25,18 @@ for (const folder of commandFolders){
     }
 }
 
-client.on(Events.InteractionCreate, async (interaction)=>{
-    if(!interaction.isChatInputCommand()) return;
-    const command = interaction.client.commands.get(interaction.commandName);
-    if(!command){
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
+const eventsPath=path.join(__dirname,'events');
+const eventFiles=fs.readdirSync(eventsPath).filter((file)=>file.endsWith('.js'));
 
-    try{
-        await command.execute(interaction);
+for (const file of eventFiles){
+    const filePath=path.join(eventsPath,file);
+    const event=require(filePath);
+    if(event.once){
+        client.once(event.name,(...args)=>event.execute(...args));
     }
-    catch(error){
-        console.error(error);
-        if(interaction.replied || interaction.deferred){
-            await interaction.followUp({
-                content: 'There was an error while executing this command!',
-                flags: MessageFlags.Ephemeral,
-            });
-        }
-        else{
-            await interaction.reply({
-                content: 'There was an error while executing this command!',
-                flags: MessageFlags.Ephemeral,
-            });
-        }
+    else{
+        client.on(event.name,(...args)=>event.execute(...args));
     }
-    // console.log(interaction);
-});
+}
 
 client.login(process.env.BOT_TOKEN);
